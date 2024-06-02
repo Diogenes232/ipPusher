8680ca87247227e2a51faa841a87481a83425015
Ubuntu <ubuntu@ip-172-31-26-124.eu-central-1.compute.internal>
2024-06-02 11:36:56 +0000

    update ec2's ip

--- getIpAndPushToGit.sh
+++ getIpAndPushToGit.sh
@@ -1,3 +1,5 @@
 curl http://checkip.amazonaws.com
 dig +short myip.opendns.com @resolver1.opendns.com
 dig +short myip.opendns.com @resolver1.opendns.com > resolvedIp.txt
+git add -A
+git commit -m "update ec2's ip"
