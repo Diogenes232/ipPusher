6ab9d2cdc7ea06251d8dd2e2160bc569a7d3fbd5
Ubuntu <ubuntu@ip-172-31-26-124.eu-central-1.compute.internal>
2024-06-02 11:27:24 +0000

    fix missing git deletions (1)

--- getIpAndPushToFtp.sh
+++
@@ -1,3 +0,0 @@
-curl http://checkip.amazonaws.com
-dig +short myip.opendns.com @resolver1.opendns.com
-dig +short myip.opendns.com @resolver1.opendns.com > myIp.txt
--- pullAndStartMovielist.sh
+++
@@ -1,4 +0,0 @@
-cd /home/ubuntu/code/movielist
-git pull
-cd /home/ubuntu/code/movielist/Filmlist_angular
-ng serve --host=0.0.0.0 --disable-host-check
