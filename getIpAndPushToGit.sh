716424890477793183e63236903dbc9302d74daa
Ubuntu <ubuntu@ip-172-31-26-124.eu-central-1.compute.internal>
2024-06-02 15:15:47 +0000

    changes

--- getIpAndPushToGit.sh
+++ getIpAndPushToGit.sh
@@ -6,9 +6,10 @@ dig +short myip.opendns.com @resolver1.opendns.com > resolvedIp.txt
 git add resolvedIp.txt
 
 # create html redirection
-echo "<html><head><title>Redirect..</title></head><body><br /><a href='http://" > resolvedRedirection.html
+echo "<html><head><title>Redirect..</title></head><body><br /><center><a href='http://" > resolvedRedirection.html
 dig +short myip.opendns.com @resolver1.opendns.com >> resolvedRedirection.html
-echo ":4200'>Redirect to EC2 instance</a></body></html>" >> resolvedRedirection.html
+echo ":4200'><font size='48px'> REDIRECT TO <br /> EC2 INSTANCE.. </font></a>" >> resolvedRedirection.html
+echo "<br /><br />avoid using AWS Elastic IP</center></body></html>" >> resolvedRedirection.html
 git add resolvedRedirection.html
 
 git commit -m "update ec2's ip"
