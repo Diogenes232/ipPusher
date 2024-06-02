b54a0adf8ba27a9814ca25b8374f3a6509ab0298
Ubuntu <ubuntu@ip-172-31-26-124.eu-central-1.compute.internal>
2024-06-02 12:00:38 +0000

    change working dir

--- getIpAndPushToGit.sh
+++ getIpAndPushToGit.sh
@@ -2,6 +2,6 @@ cd /home/ubuntu/ipPusher
 curl http://checkip.amazonaws.com
 dig +short myip.opendns.com @resolver1.opendns.com
 dig +short myip.opendns.com @resolver1.opendns.com > resolvedIp.txt
-git add -A
+git add resolvedIp.txt
 git commit -m "update ec2's ip"
 git push
