cbea2f0871aa4288777c5c667538cb8749c6ed9c
Ubuntu <ubuntu@ip-172-31-26-124.eu-central-1.compute.internal>
2024-06-02 13:06:02 +0000

    update ec2's ip

--- getIpAndPushToGit.sh
+++ getIpAndPushToGit.sh
@@ -1,7 +1,13 @@
 cd /home/ubuntu/ipPusher
 curl http://checkip.amazonaws.com
-dig +short myip.opendns.com @resolver1.opendns.com
+
+echo "<html><head><title>Redirect..</title></head><body><br /><a href='http://" > resolvedRedirection.html
+dig +short myip.opendns.com @resolver1.opendns.com >> resolvedRedirection.html
+echo ":4200'>Redirect to EC2 instance</a></body></html>" >> resolvedRedirection.html
+git add resolvedRedirection.html
+
 dig +short myip.opendns.com @resolver1.opendns.com > resolvedIp.txt
 git add resolvedIp.txt
+
 git commit -m "update ec2's ip"
 git push
