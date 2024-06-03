728d5b331917b912c885f2b7edd42f018328f919
Ubuntu <ubuntu@ip-172-31-26-124.eu-central-1.compute.internal>
2024-06-03 10:19:41 +0000

    changes

--- getIpAndPushToGit.sh
+++ getIpAndPushToGit.sh
@@ -8,7 +8,7 @@ git add resolvedIp.txt
 # create html redirection
 echo "<html><head><title>Redirect..</title></head><body><br /><center><a href='http://" > resolvedRedirection.html
 dig +short myip.opendns.com @resolver1.opendns.com >> resolvedRedirection.html
-echo ":4200'><font size='48px'> REDIRECT TO <br /> EC2 INSTANCE.. </font></a>" >> resolvedRedirection.html
+echo ":4200' target='_parent'><font size='48px'> REDIRECT TO <br /> EC2 INSTANCE.. </font></a>" >> resolvedRedirection.html
 echo "<br /><br />avoid using AWS Elastic IP</center></body></html>" >> resolvedRedirection.html
 git add resolvedRedirection.html
 
